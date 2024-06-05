4889eafe3039cdc267b05f858626baf0848a5dfa
MatanMagen <matanmagen2002@gmail.com>
2024-06-06 00:00:54 +0300

    Add new configuration files for manager, prometheus, docker, samples, rbac, and default metrics.

---
+++ api/v1alpha1/redisoperator_types.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2024 MatanMagen.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// RedisOperatorSpec defines the desired state of RedisOperator
+type RedisOperatorSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// redis version to deploy
+	RedisVersion    string `json:"redisversion"`
+	ExporterVersion string `json:"exporterVersion"`
+	TeamName        string `json:"teamName"`
+}
+
+// RedisOperatorStatus defines the observed state of RedisOperator
+type RedisOperatorStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// RedisOperator is the Schema for the redisoperators API
+type RedisOperator struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RedisOperatorSpec   `json:"spec,omitempty"`
+	Status RedisOperatorStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// RedisOperatorList contains a list of RedisOperator
+type RedisOperatorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []RedisOperator `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&RedisOperator{}, &RedisOperatorList{})
+}
---
+++ internal/controller/redisoperator_controller.go
@@ -0,0 +1,115 @@
+/*
+Copyright 2024 MatanMagen.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	apiv1alpha1 "github.com/MatanMagen/redis-operator.git/api/v1alpha1"
+)
+
+// RedisOperatorReconciler reconciles a RedisOperator object
+type RedisOperatorReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=api.core.matanmagen.io,resources=redisoperators,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=api.core.matanmagen.io,resources=redisoperators/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=api.core.matanmagen.io,resources=redisoperators/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the RedisOperator object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.2/pkg/reconcile
+func (r *RedisOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+
+	instance := &apiv1alpha1.RedisOperator{}
+	err := r.Client.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Now you can access the Spec fields
+	redisVersion := instance.Spec.RedisVersion
+	exporterVersion := instance.Spec.ExporterVersion
+	teamName := instance.Spec.TeamName
+
+	// create deplotment for redis and redis exporter
+	redisDeployment := newRedisDeployment(req.Name, req.Namespace, redisVersion, exporterVersion, teamName)
+	if err := r.Create(ctx, redisDeployment); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func newRedisDeployment(name, namespace string, redisVersion string, exporterVersion string, teamName string) *appsv1.Deployment {
+	labels := map[string]string{
+		"app": "redis",
+	}
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: func(i int32) *int32 { return &i }(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "redis",
+							Image: "redis",
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: 6379,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return dep
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *RedisOperatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&apiv1alpha1.RedisOperator{}).
+		Complete(r)
+}
